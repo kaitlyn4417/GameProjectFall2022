e6321a6d5d471c6223afda96a80ae9181259e780
kaitlyng <10957070@uvu.edu>
2022-10-20 18:06:23 -0600

    added some game objects and scriptable objects
    I literally have no idea what I'm doing

---
+++ GameProjectF22/Assets/Scripts/IngredientsSO.cs
@@ -0,0 +1,8 @@
+
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IngredientsSO : ScriptableObject
+{
+    public string ingredientName;
+}
---
+++ GameProjectF22/Assets/Scripts/SpriteBehavior.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpriteBehavior : MonoBehaviour
+{
+    public IngredientsSO ingredientName;
+}
