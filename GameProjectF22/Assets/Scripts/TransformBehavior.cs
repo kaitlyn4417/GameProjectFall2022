ba6c8b24bcd43868dcd8d267b09b6db8c52651d2
kaitlyng <10957070@uvu.edu>
2022-10-21 23:25:58 -0600

    worked on events and other scripts

--- GameProjectF22/Assets/Scripts/TransformBehavior.cs
+++ GameProjectF22/Assets/Scripts/TransformBehavior.cs
@@ -1,8 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TransformBehavior : MonoBehaviour
 {
     public Vector3 pos;
+    public UnityEvent setPositionEvent;
+
+
+    
 }
